e31da324062dadcb452440e01044bec2826fbda9
b1urrr <kristiangogow@gmail.com>
2023-02-21 10:11:30 +0200

    Info update, slight styling tweaks

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -11,14 +11,18 @@ function About() {
             <img src={avatar} alt='avatar' />
           </div>
           <div className='pf__about-container_content'>
-            <p>I've always been passionate about computers and technology. In my early teens I was crazy about gaming, later on I studied Computer Science in high school. I started dabbling in graphic design which landed me my first job as a Prepress Specialist. After that I dove in the world of networking and started my second job as a Telecommunications Technician. I then went back for a 5 year stint in design and prepress as a Desktop Publishing Operator, 2 of which in a Team Lead role</p>
+            <p>I've always been passionate about computers and technology. In my early teens I was 
+            crazy about gaming, later on I studied Computer Science in high school. I started dabbling 
+            in graphic design which landed me my first job as a Prepress Specialist. After that I dove 
+            in the world of networking and started my second job as a Telecommunications Technician. 
+            I then went back for a 5 year stint in design and prepress as a Desktop Publishing Operator, 
+            2 of which in a Team Lead role.</p>
             <br></br>
-            <p>I was always keen on learning to code, but it seemed so boring at first. Boy, was I wrong! I pushed myself into buying a course on Udemy, made a small research and decided on Python. Its easy syntax and wide range of applications were the selling point for me. I studied in pretty much all of my free time. It felt damn good to be able to manipulate the computer to your needs, not gonna lie.</p>
-            <br></br>
-            <p>Then was the disappointment. Most python developers are working with data, numbers crunching and analytics. I'm not interested in that. Where to now? I have some back-end experience - a front-end technology it is. After all I have a pretty solid design foundation. I bought another course on Udemy, this time Web Development. Same teacher, same concepts, different technology. Long story short - I devoured the course in less than a month.</p>
-            <br></br>
-            <p>Now I'm working on building a solid portfolio and putting all of my newly acquired skills and passion to use in a new position. <br></br><br></br>Stay tuned for updates! 😎</p>
-          </div>
+            <p>Now my focus is on becoming a developer. I started with Python which gave me a solid foundation
+             in understanding how code actually works and what you can do with it. Then I decided to move towards 
+             web development and learn JavaScript and it's associated libraries and frameworks, namely React.js and Node.js. 
+             I'm currently building a portfolio of projects and looking to land my first job.</p>
+            </div>
         </div>
       </div>
     )
--- src/containers/experience/imports.js
+++ src/containers/experience/imports.js
@@ -1,24 +1,31 @@
 const expItem = [
     {
+        "year": "2023-2023",
+        "role": "Full-Stack Developer",
+        "company": "Freelance",
+        "description": "Designed, developed and deployed this online portfolio, setup a CI/CD pipeline with Firebase and GitHub action deploys. Also I'm currently working on expanding my portfolio of projects and displaying them here." ,
+        "id": 1,
+    },
+    {
         "year": "2021-2023",
         "role": "Lead DTP",
         "company": "DSN Artwork Prepress",
-        "description": "As a Lead DTP my responsibilities expanded to managing a team of 5 to 10 DTP operators, aligning processes and workload with other team leaders and supervisors, analyzing and solving complex problems, ensuring top quality end product is delivered and onboarding new hires.", 
-        "id": 1,
+        "description": "As a Lead DTP my responsibilities expanded to managing a team of 5 to 10 DTP operators, aligning processes and workload with other team leaders and supervisors, analyzing and solving complex problems, ensuring top quality end product is delivered and onboarding new hires. When I got the role, the main KPI - mistake percentage went down from 10-15% to a consistent <5% which was the client benchmark.", 
+        "id": 2,
     },
     {
         "year": "2018-2021",
         "role": "Desktop Publishing Operator (DTP)",
         "company": "DSN Artwork Prepress",
         "description": "I was responsible for designing, prepressing and assuring the quality of packages for one of the biggest consumer goods companies - Unilever. I also warped sleeve type packages and briefly created 3D packshots of the products.", 
-        "id": 2,
+        "id": 3,
     },
     {
         "year": "2014-2018",
         "role": "Telecommunications Technician",
         "company": "A1 Bulgaria (formerly blizoo)",
-        "description": "Maintainance of end-user connection and equipment. I was mainly responsible for the Coaxial network, but have some experience with Fiber to the Home and Fiber to the Building technologies.", 
-        "id": 3,
+        "description": "Maintenance of end-user connection and equipment. I was mainly responsible for the Coaxial network, but have some experience with Fiber to the Home and Fiber to the Building technologies.", 
+        "id": 4,
     },
 ]
 
